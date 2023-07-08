fc372b6a5f7bc4cc506a4b1607a4f71cca091eba
unknown <jtm5432@naver.com>
2023-07-09 00:09:31 +0900

    기본 입력 틀 추가 및 id,password 검증 기능 추가

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,4 +1,109 @@
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import { useDebounce } from '../hooks';
+import { validateUserId, validatePassword } from '../auth'
 export default function SignUpForm() {
-  // TODO implement
-  return null;
+  const [userId, setUserId] = useState<string>('');
+  const [passwords, setPasswords] = useState<{ password: string; confirmPassword: string }>({ password: '', confirmPassword: '' });
+  const [enableChanges, setEnableChanges] = useState<{ ID: Boolean;password: Boolean; confirmPassword: Boolean }>({ ID:false, password: false, confirmPassword: false });
+  const [CheckResults, setCheckResults] = useState<{ ID:string | null; password: string | null; confirmPassword: string | null }>({ID:'', password: '', confirmPassword: '' });
+   
+  const debouncedUserId = useDebounce(userId, 500);
+  const debouncedPassword = useDebounce(passwords, 500);
+
+
+  useEffect(() => {
+    if (debouncedUserId && enableChanges["ID"]) {
+      console.log(`User ID: ${debouncedUserId}`);
+      setCheckResults(prevState => ({
+        ...prevState,
+        "ID":  validateUserId(`${debouncedUserId}`),
+      }));
+    }
+  }, [debouncedUserId,enableChanges["ID"]]);
+
+  useEffect(() => {
+    if (debouncedPassword && enableChanges["password"]) {
+      console.log('debouncedPassword',debouncedPassword.password)
+      setCheckResults(prevState => ({
+        ...prevState,
+        "password":  validatePassword(`${debouncedPassword.password}`),
+      }));
+    }
+  }, [debouncedPassword,enableChanges["password"]]);
+
+  useEffect(() => {
+    if (debouncedPassword && enableChanges["confirmPassword"] ){
+      let msg = (debouncedPassword.password !== debouncedPassword.confirmPassword )?  "password와 passwordConfirm이 서로 다릅니다." : '';
+      setCheckResults(prevState => ({
+        ...prevState,
+        "confirmPassword":  `${msg}`,
+      }));
+    }
+  }, [debouncedPassword,enableChanges["confirmPassword"]]);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    // Submit form logic here. For instance, you could call an API.
+    console.log(`User ID: ${userId}`);
+    console.log(`Password: ${passwords.password}`);
+  };
+  const handleInputChange = (setState: React.Dispatch<React.SetStateAction<string>>) => (event: ChangeEvent<HTMLInputElement>) => {
+    setState(event.target.value);
+    
+  };
+  const handleInputChangeArray = (inputID: keyof typeof passwords) => (event: ChangeEvent<HTMLInputElement>) => {
+    setPasswords(prevState => ({
+      ...prevState,
+      [inputID]: event.target.value,
+    }));
+  };
+  const handleFocusOut = (inputID: keyof typeof enableChanges) => (event: React.FocusEvent<HTMLInputElement>) => {
+    // Your logic here. For example:
+    console.log(`${inputID} input lost focus. Current value: ${event.target.value}`);
+  
+    setEnableChanges(prevState => ({ ...prevState, [inputID]: true }));
+   
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="userId">
+        User ID:
+        <input
+          type="text"
+          id="userId"
+          value={userId}
+          onChange={ handleInputChange(setUserId)}
+          onBlur={enableChanges.ID ?  undefined : handleFocusOut("ID")}
+        />
+      </label>
+      {CheckResults.ID && <p style={{ color: 'red' }}>{CheckResults.ID}</p>}
+      <label htmlFor="password">
+        Password:
+        <input
+          type="password"
+          id="password"
+          value={passwords.password}
+          onChange={handleInputChangeArray("password")}
+          onBlur={enableChanges.password ?  undefined : handleFocusOut("password")}
+        />
+      </label>
+      {CheckResults.password && <p style={{ color: 'red' }}>{CheckResults.password}</p>}
+      <label htmlFor="password">
+        PasswordConfirm:
+        <input
+          type="password"
+          id="confirmPassword"
+          value={passwords.confirmPassword}
+          onChange={handleInputChangeArray("confirmPassword")}
+          onBlur={enableChanges.confirmPassword ?  undefined : handleFocusOut("confirmPassword")}
+          // assuming you will add state and logic for password confirmation
+        />  
+      </label>
+      {CheckResults.confirmPassword && <p style={{ color: 'red' }}>{CheckResults.confirmPassword}</p>}
+      <button type="submit">Register</button>
+    </form>
+  );
+
+
+
 }
---
+++ src/components/auth.ts
@@ -0,0 +1,36 @@
+type ValidationFn = (value: string) => string | null;
+
+const minLength = (length: number): ValidationFn => (value: string) =>
+  value.length >= length ? null : `${length}자 이상이어야 합니다`;
+
+const maxLength = (length: number): ValidationFn => (value: string) =>
+  value.length <= length ? null : `${length}자 이하이어야 합니다`;
+
+const containsDigit: ValidationFn = (value: string) =>
+  /\d/.test(value) ? null : '숫자를 포함해야 합니다';
+
+const containsLowercase: ValidationFn = (value: string) =>
+  /[a-z]/.test(value) ? null : '소문자를 포함해야 합니다';
+
+const containsUppercase: ValidationFn = (value: string) =>
+  /[A-Z]/.test(value) ? null : '대문자를 포함해야 합니다';
+
+export const validateUserId = (userId: string): string | null => {
+  return (
+    minLength(1)(userId) ||
+    maxLength(10)(userId) ||
+    containsDigit(userId) ||
+    containsLowercase(userId) ||
+    containsUppercase(userId)
+  );
+};
+
+export const validatePassword = (password: string): string | null => {
+  return (
+    minLength(8)(password) ||
+    maxLength(10)(password) ||
+    containsDigit(password) ||
+    containsLowercase(password) ||
+    containsUppercase(password)
+  );
+};
